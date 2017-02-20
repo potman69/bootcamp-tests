1fe9715cd091283577b33a9d75ba94d604bfade9
potman69 <anton@twk.co.za>
2017-02-20 15:49:44 +0200

    First commit

---
+++ test/greet.test.js
@@ -0,0 +1,7 @@
+var assert = chai.assert
+
+describe('should test for my greet function',function(){
+  it('should test for my greet function',function(){
+    assert.equal(greet('Janine'),'Hello, Janine')
+  })
+})
