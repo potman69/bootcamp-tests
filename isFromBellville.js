f0a916439ec05b7cad422bc9b69b41932d69cc07
potman69 <anton@twk.co.za>
2017-02-21 09:39:05 +0200

    update

---
+++ isFromBellville.js
@@ -0,0 +1,7 @@
+function isFromBellville(Bellville){
+var fromBelville = Bellville.startsWith("CY")
+return fromBelville;
+}
+
+var result = isFromBellville("CY");
+console.log(result);
---
+++ test/isFromBellville.test.js
@@ -0,0 +1,7 @@
+var assert = chai.assert
+
+describe('should test for my isFromBellville function',function(){
+  it('should test for my isFromBellville function',function(){
+    assert.equal(isFromBellville('Bellville'),'true')
+  })
+})
