f519ef691b08ddcaf0c35094c8394b2ebbee5a05
potman69 <anton@twk.co.za>
2017-02-22 13:00:27 +0200

    done.

--- test/isFromBellville.test.js
+++ test/isFromBellville.test.js
@@ -2,6 +2,6 @@ var assert = chai.assert
 
 describe('should test for my isFromBellville function',function(){
   it('should test for my isFromBellville function',function(){
-    assert.equal(isFromBellville('CY'),'true')
+    assert.equal(isFromBellville('result'),'true')
   })
 })
