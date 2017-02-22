9e50e6a9e399e95a70e8e085a29fcc76eb04e5d8
potman69 <anton@twk.co.za>
2017-02-22 13:18:46 +0200

    done.

--- isFromBellville.js
+++ isFromBellville.js
@@ -2,6 +2,3 @@ function isFromBellville(Bellville){
 var fromBelville = Bellville.startsWith("CY")
 return fromBelville;
 }
-
-var result = isFromBellville("CY");
-console.log(result);
--- test/isFromBellville.test.js
+++ test/isFromBellville.test.js
@@ -2,6 +2,6 @@ var assert = chai.assert
 
 describe('should test for my isFromBellville function',function(){
   it('should test for my isFromBellville function',function(){
-    assert.equal(isFromBellville('result'),'true')
+    assert.equal(isFromBellville('CY 8347'),'true')
   })
 })
